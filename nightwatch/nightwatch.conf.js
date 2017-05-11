dc30aedae8d7843b98f33244739953b68c58bb9d
glazar <gabriel.lazar@3pillarglobal.com>
2017-05-11 16:40:39 +0300

    Make tests run in parallel: multiple browsers & multiple cores

--- nightwatch/nightwatch.conf.js
+++ nightwatch/nightwatch.conf.js
@@ -1,4 +1,8 @@
 module.exports = (function(settings) {
   //settings.selenium.start_process = false;
+  //settings.test_workers = {
+  //  "enabled": true,
+  //  "workers": "auto"
+  //};
   return settings;
 })(require('./nightwatch.json'));
