2dc2c7fec6892100c8983d04dd3f166b030d5623
glazar <gabriel.lazar@3pillarglobal.com>
2017-05-22 16:57:00 +0300

    Add in gitignore files to have empty folders present for clear runs

--- nightwatch/tests/google-basic.js
+++ nightwatch/tests/google-basic.js
@@ -8,7 +8,7 @@ module.exports = {
       .setValue('input[type=text]', 'nightwatch')
       .waitForElementVisible('button[name=btnG]', 1000)
       .click('button[name=btnG]')
-      .pause(1000)
+      .pause(2000)
       .assert.containsText('#main', 'Night Watch');
   },
 
