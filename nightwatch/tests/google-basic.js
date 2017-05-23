889ce67bba8a1eee4168ca05ad207b74cfac96bb
glazar <gabriel.lazar@3pillarglobal.com>
2017-05-23 10:49:49 +0300

    Remove comments from nightwatch example test

--- nightwatch/tests/google-basic.js
+++ nightwatch/tests/google-basic.js
@@ -17,19 +17,10 @@ module.exports = {
       .url('http://google.com')
       .pause(1000);
 
-    // expect element  to be present in 1000ms
     client.expect.element('body').to.be.present.before(1000);
-
-    // expect element <#lst-ib> to have css property 'display'
     client.expect.element('#lst-ib').to.have.css('display');
-
-    // expect element  to have attribute 'class' which contains text 'vasq'
     client.expect.element('body').to.have.attribute('class').which.contains('vasq');
-
-    // expect element <#lst-ib> to be an input tag
     client.expect.element('#lst-ib').to.be.an('input');
-
-    // expect element <#lst-ib> to be visible
     client.expect.element('#lst-ib').to.be.visible;
   },
 
