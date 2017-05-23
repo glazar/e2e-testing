7cdfa08b022371c7529818cbe3788b6a1b631555
glazar <gabriel.lazar@3pillarglobal.com>
2017-05-23 14:27:59 +0300

    Allow for reports to work cross platform

--- cucumber/support/event-handlers.js
+++ cucumber/support/event-handlers.js
@@ -1,12 +1,13 @@
 const {defineSupportCode} = require('cucumber');
 const reporter = require('cucumber-html-reporter');
+var path = require('path');
 
 defineSupportCode(({registerHandler}) => {
   registerHandler('AfterFeatures', () => {
     reporter.generate({
       theme: 'bootstrap',
-      jsonFile: 'cucumber/reports/cucumber.json',
-      output: 'cucumber/reports/cucumber-report.html',
+      jsonFile: path.join('cucumber', 'reports', 'cucumber.json'),
+      output:  path.join('cucumber', 'reports', 'cucumber-report.html'),
       reportSuiteAsScenarios: true,
       launchReport: false
     });
--- nightwatch-cucumber/support/event-handlers.js
+++ nightwatch-cucumber/support/event-handlers.js
@@ -1,12 +1,13 @@
 const {defineSupportCode} = require('cucumber');
 const reporter = require('cucumber-html-reporter');
+var path = require('path');
 
 defineSupportCode(({registerHandler}) => {
   registerHandler('AfterFeatures', () => {
     reporter.generate({
       theme: 'bootstrap',
-      jsonFile: 'nightwatch-cucumber/reports/cucumber.json',
-      output: 'nightwatch-cucumber/reports/nightwatch-cucumber-report.html',
+      jsonFile: path.join('nightwatch-cucumber', 'reports', 'cucumber.json'),
+      output:  path.join('nightwatch-cucumber', 'reports', 'nightwatch-cucumber-report.html'),
       reportSuiteAsScenarios: true,
       launchReport: false
     });
